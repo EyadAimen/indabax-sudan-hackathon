7efc21249873bfbcd1430d030f71a3b2233290df
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2025-12-11 23:40:24 +0800

    navbar bug

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@ import styles from "./header.module.css";
 import logo from "../../assets/ixs-logo.png";
 import { useState, useEffect } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import Link from "next/link";
 
 type HeaderProps = {
     activeID?: number;
@@ -46,10 +47,10 @@ function Header ({ activeID }: HeaderProps) {
     }
     
     const tracks = [
-        {id: 1, name: "track 1", url: "track1"},
-        {id: 2, name: "track 2", url: "track2"},
-        {id: 3, name: "track 3", url: "track3"},
-        {id: 4, name: "track 4", url: "track4"}
+        {id: 1, name: "track 1", url: "/track1"},
+        {id: 2, name: "track 2", url: "/track2"},
+        {id: 3, name: "track 3", url: "/track3"},
+        {id: 4, name: "track 4", url: "/track4"}
     ]
 
     
@@ -61,14 +62,14 @@ function Header ({ activeID }: HeaderProps) {
             <div className={styles.headerItems}>
 
                 <div className={styles.logoContainer}>
-                  <a href="/" className={styles.logoLink}>
+                  <Link href="/" className={styles.logoLink}>
                     <Image 
                         className={styles.image}
                         src={logo} 
                         alt="IndabaX Sudan logo"
                     />
                     <p className={styles.logoText}>IndabaX Sudan 2025</p>
-                  </a>
+                  </Link>
                 </div>
 
                 {/* nav bar links */}
@@ -84,7 +85,7 @@ function Header ({ activeID }: HeaderProps) {
                     ):
                     (
                         <ul className={styles.navBar}>
-                          {tracks.map((track) => <li className={activeID === track.id ? styles.activeNavBarItem : styles.navBarItem}><a href={track.url}>{track.name}</a></li>)}
+                          {tracks.map((track) => <li className={activeID === track.id ? styles.activeNavBarItem : styles.navBarItem}><Link href={track.url}>{track.name}</Link></li>)}
                         </ul>
                     )
                 
@@ -94,7 +95,7 @@ function Header ({ activeID }: HeaderProps) {
             </div>
             {  isMobile && (
                 <div className={`${styles.navBarItems} ${isMenuHidden ? styles.hidden : styles.visible}`} >
-                    {tracks.map((track) => <a href={track.url}><li className={activeID === track.id ? styles.activeNavBarItemMobile : styles.navBarItemMobile}>{track.name}</li></a>)}
+                    {tracks.map((track) => <Link href={track.url}><li className={activeID === track.id ? styles.activeNavBarItemMobile : styles.navBarItemMobile}>{track.name}</li></Link>)}
                 </div>
             )}
       </header>
