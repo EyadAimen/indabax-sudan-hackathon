470b5b5bc0126f35c1303e5d91fc9938e9d8a10c
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2025-12-16 09:19:47 +0800

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   basePath: "/indabax-sudan-hackathon",
   images: {
     unoptimized: true,
