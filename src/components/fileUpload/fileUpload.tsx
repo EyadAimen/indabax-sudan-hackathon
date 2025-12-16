e99f3aa99a915a6618a1acc9d40831dfff7bd28f
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2025-12-16 08:56:59 +0800

    ...

--- src/components/fileUpload/fileUpload.tsx
+++ src/components/fileUpload/fileUpload.tsx
@@ -44,7 +44,7 @@ const FileUpload: React.FC<FileUploadProps> = ({
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await fetch('/indabax-sudan-hackathon/api/upload', {
+      const response = await fetch('/api/upload', {
         method: 'POST',
         body: formData,
       });
